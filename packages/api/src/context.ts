c98dc4f2f1757fbb1430e5cdc637100fc78267c2
Dominik Zajac <dominik.zajac@kruko.io>
2024-01-19 00:17:33 +0100

    Revert "update clerk package"
    This reverts commit 602812d33d4fc4ef1e47c41134486c500034a05d.

--- packages/api/src/context.ts
+++ packages/api/src/context.ts
@@ -31,7 +31,6 @@ export const createContextInner = async ({ auth }: AuthContextProps) => {
  * @link https://trpc.io/docs/context
  **/
 export const createContext = async (opts: CreateNextContextOptions) => {
-  console.log("createContext", opts.req);
   return await createContextInner({ auth: getAuth(opts.req) });
 };
 
